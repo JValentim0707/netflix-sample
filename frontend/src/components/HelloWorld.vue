a6ad3c044b43ea98f29063587547a719256c0d36
Romano777777 <br0567575@gmail.com>
2022-11-23 20:48:33 -0300

    Romano update

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -14,13 +14,13 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h1>Romano</h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Bacon</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">comida</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">pessoa</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">carne</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">prato</a></li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
